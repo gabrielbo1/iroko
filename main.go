25eefecb9c40fb92a8ff2b5526e1956cb00a7fbe
gabriel <goliveira@atentatecnologia.com.br>
2020-01-31 15:00:45 -0300

    #1 :arrow_right: Hello work heroku app.

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
 
 func main() {
-	fmt.Println("Hello word")
+    port := os.Getenv("PORT") // Heroku provides the port to bind to
+    if port == "" {
+      port = "8080"
+    }
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":" + port, nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, word!")
 }
