718d7ad06086825d1c1f68fc4692c0da3955d9c6
Gabriel Barbosa de Oliveira <barbosa.olivera1@gmail.com>
2020-01-31 15:27:50 -0300

    #1 :arrow_right: Hello work heroku app. (#2)
    * #1 :arrow_right: Hello work heroku app.
    
    * #1 :arrow_right: Code review funcion.

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func helloServer(w http.ResponseWriter, r *http.Request) {
+   fmt.Fprint(w, "Hello, word!")
+}
 
 func main() {
-	fmt.Println("Hello word")
+    port := os.Getenv("PORT") // Heroku provides the port to bind to
+    if port == "" {
+      port = "8080"
+    }
+    http.HandleFunc("/", helloServer)
+    http.ListenAndServe(":" + port, nil)
 }
