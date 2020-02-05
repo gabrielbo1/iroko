4d64dcf8abf3bb8b7db64ff33d366b34faf187ab
gabriel <goliveira@atentatecnologia.com.br>
2020-02-05 18:40:22 -0300

    Consul integration

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gabrielbo1/iroko
 require (
 	github.com/gorilla/mux v1.7.3
 	github.com/hashicorp/consul v1.5.3
+	github.com/hashicorp/consul/api v1.1.0
 	github.com/jinzhu/now v1.1.1
 	github.com/lib/pq v1.3.0
 )
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gabrielbo1/iroko/config"
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/connect"
 )
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
@@ -13,5 +16,28 @@ func helloServer(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", helloServer)
+
+	if config.EnvironmentVariableValue(config.ConsulActive) == "true" {
+		// build client
+		configConsul := api.DefaultConfig()
+		configConsul.Address = config.EnvironmentVariableValue(config.ConsulAddress)
+		configConsul.Address += ":" + config.EnvironmentVariableValue(config.ConsulPort)
+
+		clientConsul, err := api.NewClient(configConsul)
+		if err != nil {
+			log.Fatalf("client err: %v", err)
+		}
+
+		svc, _ := connect.NewService("iroko-service", clientConsul)
+		defer svc.Close()
+
+		// Creating an HTTP server that serves via Connect
+		server := &http.Server{
+			Addr:      ":" + config.EnvironmentVariableValue(config.Port),
+			TLSConfig: svc.ServerTLSConfig(),
+		}
+		server.ListenAndServeTLS("", "")
+	}
+
 	http.ListenAndServe(":"+config.EnvironmentVariableValue(config.Port), nil)
 }
