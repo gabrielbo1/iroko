326a5477a3d796cd656257a7afd58f80efb96533
Gabriel Barbosa de Oliveira <barbosa.olivera1@gmail.com>
2020-03-04 00:55:58 -0300

    Create project web server (#27)
    * Go micro and PostgreSQL and Now lib golang.  :construction:
    
    * Add consul lib and EnvironmentVariable implementation.  :construction:
    
    * Consul integration
    
    * Consul config.
    
    * JWT Token.
    
    * Delete .vscode :see_no_evil:
    
    * Front-End folder root project. :building_construction: :construction_worker:
    
    * #13 Git Ignore Front-End project :see_no_evil:.
    
    * #13 Front-End project. :building_construction:
    
    * #13 Front-End project config pwa build and manifest.json. :building_construction:
    
    * #13 Front-End build. :building_construction: :construction_worker: :whale:
    
    * #13 Back-End JWT Token adjust authentication and add path prefix with /api/APP_NAME environment. :construction:
    
    * #13 Front-End build bug. :construction_worker: :whale: :bug:
    
    * #13 Front-End build bug. :construction_worker: :whale: :bug:
    
    * #13 Front-End build :construction_worker:
    
    * #13 Front-End build :construction_worker:
    
    * #13 Front-End build :construction_worker:
    
    * Run npm audit :whale: :construction_worker:.
    
    * Build.
    
    * Build Front-End :construction_worker: :whale:.
    
    * Build Front-End :construction_worker: :whale:.
    
    * Build Front-End :construction_worker: :whale:.
    
    * Removing go linter warnings. :rotating_light:
    
    * Heroku :construction_worker:

--- build/build_amd64.sh
+++ build/build_amd64.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eval "docker tag gabrielbo1/iroko-$TRAVIS_CPU_ARCH:${TAG} registry.heroku.com/$HEROKU_APP_NAME/web"
+eval "docker tag gabrielbo1/iroko-$TRAVIS_CPU_ARCH registry.heroku.com/$HEROKU_APP_NAME/web"
