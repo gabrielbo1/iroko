d74d92d7afcb426371cc1c056033a321f3f7f609
gabriel <goliveira@atentatecnologia.com.br>
2020-04-18 05:25:42 -0300

    #12 Layout base project. :rocket: :sparkles: :art:

--- website/iroko-app/src/app/app-routing.module.ts
+++ website/iroko-app/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AuthenticateService} from "./AuthenticateService";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login', component: LoginComponent
+  },
+  {
+    path: 'pages', loadChildren: 'src/app/pages/pages.module#PagesModule',
+    canActivate: [AuthenticateService]
+  },
+  {
+    path: '',
+    redirectTo: 'pages',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'pages',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- website/iroko-app/src/app/app.module.ts
+++ website/iroko-app/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import {AuthenticateService} from "./AuthenticateService";
+import {MaterialModule} from "./material.module";
+import {MatNativeDateModule} from "@angular/material/core";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    MatNativeDateModule,
   ],
-  providers: [],
+  providers: [
+    AuthenticateService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
